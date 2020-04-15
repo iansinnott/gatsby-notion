15a45ef8c3e4f2d47792f2dd13779670683bc29b
Ian Sinnott <ian@iansinnott.com>
2020-04-15 17:05:20 +0800

    Add gatsby notion databaes plugin

--- packages/gatsby-notion-example/gatsby-config.js
+++ packages/gatsby-notion-example/gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-source-notion-database",
+      options: {
+        name: "Quirky",
+        databaseViewUrl:
+          "https://www.notion.so/iansinnott/c6001aa9780c4240a219fa5819773e99?v=49e37d3868a14396b05c17440bf05966",
+        debug: true
+      }
+    },
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`
   ]
