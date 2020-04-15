fe8487888258a17bb8e39affdc01dc1787b1f91f
Ian Sinnott <ian@iansinnott.com>
2020-04-15 16:36:37 +0800

    More updates to boilerplate

--- packages/gatsby-notion-example/gatsby-config.js
+++ packages/gatsby-notion-example/gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Typescript Starter`,
+    title: `Gatsby Notion Example`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`
-  ],
-}
+  ]
+};
