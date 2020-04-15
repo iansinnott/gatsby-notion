7eef0d77c9e1dc3ece2dc18001efe478b88ac9cd
Ian Sinnott <ian@iansinnott.com>
2020-04-15 16:32:49 +0800

    Update example package.json and run format

--- packages/gatsby-notion-example/src/layouts/index.tsx
+++ packages/gatsby-notion-example/src/layouts/index.tsx
@@ -1,29 +1,29 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import * as React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import './index.css'
+import "./index.css";
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: "rebeccapurple",
+      marginBottom: "1.45rem",
     }}
   >
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: "1.45rem 1.0875rem",
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            color: "white",
+            textDecoration: "none",
           }}
         >
           Gatsby
@@ -31,13 +31,13 @@ const Header = () => (
       </h1>
     </div>
   </div>
-)
+);
 
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
   location: {
-    pathname: string
-  }
-  children: any
+    pathname: string;
+  };
+  children: any;
 }
 
 class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
@@ -47,24 +47,24 @@ class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
         <Helmet
           title="Gatsby Default Starter"
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: "description", content: "Sample" },
+            { name: "keywords", content: "sample, something" },
           ]}
         />
         <Header />
         <div
           style={{
-            margin: '0 auto',
+            margin: "0 auto",
             maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
+            padding: "0px 1.0875rem 1.45rem",
             paddingTop: 0,
           }}
         >
           {this.props.children()}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default DefaultLayout
+export default DefaultLayout;
