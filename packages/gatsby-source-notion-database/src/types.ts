86c7f36158c260565f8f2b5da1c9c4878775f97a
Ian Sinnott <ian@iansinnott.com>
2020-04-22 16:23:41 +0800

    Update types

--- packages/gatsby-source-notion-database/src/types.ts
+++ packages/gatsby-source-notion-database/src/types.ts
@@ -1,4 +1,29 @@
 import { PluginOptions } from 'gatsby';
+import { Block } from 'notionapi-agent/dist/interfaces';
+import { BasicBlockUnion } from 'notionapi-agent/dist/interfaces/notion-models/block/basic_block';
+import { MediaBlockUnion } from 'notionapi-agent/dist/interfaces/notion-models/block/media';
+import { SemanticString } from 'notionapi-agent/dist/interfaces';
+
+type NotionPropertyValue = Array<[string, any?]>;
+
+type NotionBlock = BasicBlockUnion | MediaBlockUnion;
+
+interface PropertyDetails {
+  pid: string;
+  value: string;
+  _raw: string;
+  name: string;
+  type: string;
+}
+
+type CollectionBlock = Block & {
+  properties: { [k: string]: NotionPropertyValue };
+};
+
+interface DateObject {
+  type: 'date';
+  start_date: string;
+}
 
 export interface NotionMeta {
   slug?: string;
@@ -101,3 +126,54 @@ export interface NotionLoader {
   getBlocks(copyTo: NotionPageBlock[], pageId: string): void;
   reset(): void;
 }
+
+export interface BlockType {
+  id: string;
+  version: number;
+  type: string;
+  properties: { [k: string]: any };
+  content?: {
+    id: string;
+    version: number;
+    type: string;
+    properties: {
+      title?: (string | string[][])[][];
+      language?: string[][];
+      source?: string[][];
+      caption?: (string | string[][])[][];
+    };
+    created_time: number;
+    last_edited_time: number;
+    parent_id: string;
+    parent_table: string;
+    alive: boolean;
+    created_by_table: string;
+    created_by_id: string;
+    last_edited_by_table: string;
+    last_edited_by_id: string;
+    format?: {
+      block_width: number;
+      display_source: string;
+      block_full_width: boolean;
+      block_page_width: boolean;
+      block_aspect_ratio: number;
+      block_preserve_scale: boolean;
+    };
+    file_ids?: string[];
+  }[];
+  created_time: number;
+  last_edited_time: number;
+  parent_id: string;
+  parent_table: string;
+  alive: boolean;
+  created_by_table: string;
+  created_by_id: string;
+  last_edited_by_table: string;
+  last_edited_by_id: string;
+  _notionBlockId: string;
+  _propertyDetails: { [k: string]: PropertyDetails };
+}
+
+export { IntermediateForm } from './mapIntermediateContentRepresentation';
+
+export type Unpacked<T> = T extends (infer U)[] ? U : T;
